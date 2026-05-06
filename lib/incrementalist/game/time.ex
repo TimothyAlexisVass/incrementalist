59fee095a2613449d2cf4fbf166c2f7e8edfc0c2
TimothyAlexisVass <55708319+TimothyAlexisVass@users.noreply.github.com>
2026-05-06 11:37:01 +0000

    Fix time helper methods in Elixir logic
    Added missing `from_iso8601` and `to_unix_ms` functions to `Incrementalist.Game.Time` module to support the timestamp manipulation for parsing strings to valid ISO integers and calculating progress delays.

--- lib/incrementalist/game/time.ex
+++ lib/incrementalist/game/time.ex
@@ -5,4 +5,15 @@ defmodule Incrementalist.Game.Time do
 
   def iso8601(%DateTime{} = datetime), do: DateTime.to_iso8601(datetime)
   def iso8601(nil), do: nil
+
+  def from_iso8601(iso_str) when is_binary(iso_str) do
+    case DateTime.from_iso8601(iso_str) do
+      {:ok, dt, _offset} -> {:ok, dt}
+      error -> error
+    end
+  end
+
+  def to_unix_ms(%DateTime{} = datetime) do
+    DateTime.to_unix(datetime, :millisecond)
+  end
 end
